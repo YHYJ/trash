44c969ec123110d4b5ee48e47c983d8594e48434
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2023-11-29 11:45:08 +0800

    添加通用组件

---
+++ general/define_filemanager.go
@@ -0,0 +1,412 @@
+/*
+File: define_filemanager.go
+Author: YJ
+Email: yj1516268@outlook.com
+Created Time: 2023-11-26 11:32:05
+
+Description: 文件管理
+*/
+
+package general
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+)
+
+// ReadFileLine 读取文件指定行
+//
+// 参数：
+//   - file: 文件路径
+//   - line: 行号
+//
+// 返回：
+//   - 指定行的内容
+func ReadFileLine(file string, line int) string {
+	// 打开文件
+	text, err := os.Open(file)
+	if err != nil {
+		log.Println(err)
+	}
+	defer text.Close()
+
+	// 创建一个扫描器对象按行遍历
+	scanner := bufio.NewScanner(text)
+	// 行计数
+	count := 1
+	// 逐行读取，输出指定行
+	for scanner.Scan() {
+		if line == count {
+			return scanner.Text()
+		}
+		count++
+	}
+	return ""
+}
+
+// ReadFileKey 读取文件包含关键字的行
+//
+// 参数：
+//   - file: 文件路径
+//   - key: 关键字
+//
+// 返回：
+//   - 包含关键字的行的内容
+func ReadFileKey(file, key string) string {
+	// 打开文件
+	text, err := os.Open(file)
+	if err != nil {
+		log.Println(err)
+	}
+	defer text.Close()
+
+	// 创建一个扫描器对象按行遍历
+	scanner := bufio.NewScanner(text)
+	// 逐行读取，输出指定行
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), key) {
+			return scanner.Text()
+		}
+	}
+	return ""
+}
+
+// ReadFileCount 获取文件包含关键字的行的计数
+//
+// 参数：
+//   - file: 文件路径
+//   - key: 关键字
+//
+// 返回：
+//   - 包含关键字的行的数量
+func ReadFileCount(file, key string) int {
+	// 打开文件
+	text, err := os.Open(file)
+	if err != nil {
+		log.Println(err)
+	}
+	defer text.Close()
+
+	// 创建一个扫描器对象按行遍历
+	scanner := bufio.NewScanner(text)
+	// 计数器
+	count := 0
+	// 逐行读取，输出指定行
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), key) {
+			count++
+		}
+	}
+	return count
+}
+
+// FileExist 判断文件是否存在
+//
+// 参数：
+//   - filePath: 文件路径
+//
+// 返回：
+//   - 文件存在返回 true，否则返回 false
+func FileExist(filePath string) bool {
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+// GetAbsPath 获取指定文件的绝对路径
+//
+// 参数：
+//   - filePath: 文件路径
+//
+// 返回：
+//   - 文件的绝对路径
+func GetAbsPath(filePath string) string {
+	// 获取绝对路径
+	absPath, err := filepath.Abs(filePath)
+	if err != nil {
+		return ""
+	} else {
+		return absPath
+	}
+}
+
+// FileEmpty 判断文件是否为空
+//
+//   - 无法判断文件夹
+//
+// 参数：
+//   - filePath: 文件路径
+//
+// 返回：
+//   - 文件为空返回 true，否则返回 false
+func FileEmpty(filePath string) bool {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return true
+	}
+	defer file.Close()
+
+	fi, err := file.Stat()
+	if err != nil {
+		return true
+	}
+	return fi.Size() == 0
+}
+
+// FolderEmpty 判断文件夹是否为空
+//
+//   - 包括隐藏文件
+//
+// 参数：
+//   - filePath: 文件夹路径
+//
+// 返回：
+//   - 文件夹为空返回 true，否则返回 false
+func FolderEmpty(filePath string) bool {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return true
+	}
+	defer file.Close()
+
+	_, err = file.Readdir(1)
+	if err == io.EOF {
+		return true
+	}
+	return false
+}
+
+// CreateFile 创建文件，包括其父目录
+//
+// 参数：
+//   - filePath: 文件路径
+//
+// 返回：
+//   - 错误信息
+func CreateFile(filePath string) error {
+	if FileExist(filePath) {
+		return nil
+	}
+	// 创建父目录
+	parentPath := filepath.Dir(filePath)
+	if err := os.MkdirAll(parentPath, os.ModePerm); err != nil {
+		return err
+	}
+	// 创建文件
+	if _, err := os.Create(filePath); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// CreateDir 创建文件夹
+//
+// 参数：
+//   - dirPath: 文件夹路径
+//
+// 返回：
+//   - 错误信息
+func CreateDir(dirPath string) error {
+	if FileExist(dirPath) {
+		return nil
+	}
+	return os.MkdirAll(dirPath, os.ModePerm)
+}
+
+// GoToDir 进到指定文件夹
+//
+// 参数：
+//   - dirPath: 文件夹路径
+//
+// 返回：
+//   - 错误信息
+func GoToDir(dirPath string) error {
+	return os.Chdir(dirPath)
+}
+
+// WriteFile 写入内容到文件
+//
+// 参数：
+//   - filePath: 文件路径
+//   - content: 内容
+//
+// 返回：
+//   - 错误信息
+func WriteFile(filePath string, content string) error {
+	// 文件存在
+	if FileExist(filePath) {
+		// 文件内容为空
+		if FileEmpty(filePath) {
+			// 打开文件并写入内容
+			file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_TRUNC, 0666)
+			if err != nil {
+				return err
+			} else {
+				_, err := file.WriteString(content)
+				if err != nil {
+					return err
+				}
+			}
+		} else {
+			// 文件内容不为空
+			return fmt.Errorf("File %s is not empty", filePath)
+		}
+	} else {
+		// 文件不存在，创建文件
+		if err := CreateFile(filePath); err != nil {
+			return err
+		}
+		// 打开文件并写入内容
+		file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_TRUNC, 0666)
+		if err != nil {
+			return err
+		} else {
+			_, err := file.WriteString(content)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// DeleteFile 删除文件
+//
+// 参数：
+//   - filePath: 文件路径
+//
+// 返回：
+//   - 错误信息
+func DeleteFile(filePath string) error {
+	if !FileExist(filePath) {
+		return nil
+	}
+	return os.Remove(filePath)
+}
+
+// CompareFile 并发比较两个文件是否相同
+//
+// 参数：
+//   - file1Path: 文件1路径
+//   - file2Path: 文件2路径
+//
+// 返回：
+//   - 文件相同返回 true，出错或不同返回 false
+func CompareFile(file1Path string, file2Path string) (bool, error) {
+	// 尝试打开文件
+	file1, err := os.Open(file1Path)
+	if err != nil {
+		return false, err
+	}
+	defer file1.Close()
+	file2, err := os.Open(file2Path)
+	if err != nil {
+		return false, err
+	}
+	defer file2.Close()
+
+	// 获取文件大小
+	file1Info, err := file1.Stat()
+	if err != nil {
+		return false, err
+	}
+	file2Info, err := file2.Stat()
+	if err != nil {
+		return false, err
+	}
+	file1Size := file1Info.Size()
+	file2Size := file2Info.Size()
+
+	// 如果文件大小不同则直接返回
+	if file1Size != file2Size {
+		return false, nil
+	}
+
+	// 文件大小相同则（按块）比较文件内容
+	const chunkSize = 1024 * 1024                             // 每次比较的块大小（1MB）
+	numChunks := int((file1Size + chunkSize - 1) / chunkSize) // 计算文件需要被分成多少块
+
+	equal := true                // 文件是否相同的标志位
+	var wg sync.WaitGroup        // wg 用于等待所有的 goroutine 执行完毕，然后关闭 errCh 通道
+	errCh := make(chan error, 1) // errCh 用于接收 goroutine 执行过程中返回的错误
+
+	for i := 0; i < numChunks; i++ { // 同时比较多个块
+		wg.Add(1)
+		go func(chunkIndex int) {
+			defer wg.Done()
+
+			// 计算当前块的偏移量和大小
+			offset := int64(chunkIndex) * chunkSize
+			size := chunkSize
+			if offset+int64(size) > file1Size {
+				size = int(file1Size - offset)
+			}
+
+			// 创建两个大小为 size 的 buffer
+			buffer1 := make([]byte, size)
+			buffer2 := make([]byte, size)
+
+			// 从文件中读取指定大小的内容到 buffer
+			_, err := file1.ReadAt(buffer1, offset)
+			if err != nil && err != io.EOF {
+				errCh <- err
+				return
+			}
+
+			// 从文件中读取指定大小的内容到 buffer
+			_, err = file2.ReadAt(buffer2, offset)
+			if err != nil && err != io.EOF {
+				errCh <- err
+				return
+			}
+
+			// 比较两个 buffer 是否相同
+			if !bytesEqual(buffer1, buffer2) {
+				equal = false
+			}
+		}(i)
+	}
+
+	go func() {
+		wg.Wait()
+		close(errCh)
+	}()
+
+	for err := range errCh {
+		if err != nil {
+			return false, err
+		}
+	}
+
+	return equal, nil
+}
+
+// bytesEqual 比较两个文件的内容
+//
+// 参数：
+//   - b1: 文件1内容
+//   - b2: 文件2内容
+//
+// 返回：
+//   - 相同返回 true，不同返回 false
+func bytesEqual(b1 []byte, b2 []byte) bool {
+	if len(b1) != len(b2) {
+		return false
+	}
+
+	for i := 0; i < len(b1); i++ {
+		if b1[i] != b2[i] {
+			return false
+		}
+	}
+
+	return true
+}
---
+++ general/define_variable.go
@@ -0,0 +1,172 @@
+/*
+File: define_variable.go
+Author: YJ
+Email: yj1516268@outlook.com
+Created Time: 2023-11-26 11:32:05
+
+Description: 操作变量（包括代码变量和环境变量）
+*/
+
+package general
+
+import (
+	"os"
+	"os/user"
+	"runtime"
+	"strconv"
+)
+
+// ---------- 代码变量
+
+var (
+	RegelarFormat   = "%s\n"   // 常规输出格式 常规输出: <输出内容>
+	Regelar2PFormat = "%s%s\n" // 常规输出格式 常规输出·2部分: <输出内容1><输出内容2>
+
+	TitleH1Format = "\n\x1b[36;3m%s\x1b[0m\n\n" // 标题输出格式 H1级别标题: <标题文字>
+
+	LineHiddenFormat = "\x1b[30m%s\x1b[0m\n"   // 分隔线输出格式 隐性分隔线: <分隔线>
+	LineShownFormat  = "\x1b[30;1m%s\x1b[0m\n" // 分隔线输出格式 显性分隔线: <分隔线>
+
+	SliceTraverseFormat                  = "\x1b[32;1m%s\x1b[0m\n"                                                                                 // Slice输出格式 切片遍历: <切片内容>
+	SliceTraverseSuffixFormat            = "\x1b[32;1m%s\x1b[0m%s%s\n"                                                                             // Slice输出格式 带后缀的切片遍历: <切片内容><分隔符><后缀>
+	SliceTraverse2PFormat                = "\x1b[32;1m%s\x1b[0m%s\x1b[34m%s\x1b[0m\n"                                                              // Slice输出格式 切片遍历·2部分: <切片内容1><分隔符><切片内容2>
+	SliceTraverse2PSuffixFormat          = "\x1b[32;1m%s\x1b[0m%s\x1b[34m%s\x1b[0m%s%s\n"                                                          // Slice输出格式 带后缀的切片遍历·2部分: <切片内容1><分隔符><切片内容2><分隔符><后缀>
+	SliceTraverse2PSuffixNoNewLineFormat = "\x1b[32;1m%s\x1b[0m%s\x1b[34m%s\x1b[0m%s%s"                                                            // Slice输出格式 带后缀的切片遍历·2部分·不换行: <切片内容1><分隔符><切片内容2><分隔符><后缀>
+	SliceTraverse3PSuffixFormat          = "\x1b[32;1m%s\x1b[0m%s\x1b[34m%s\x1b[0m%s\x1b[33;1m%s\x1b[0m%s%s\n"                                     // Slice输出格式 带后缀的切片遍历·3部分: <切片内容1><分隔符><切片内容2><分隔符><切片内容3><分隔符><后缀>
+	SliceTraverse4PFormat                = "\x1b[32;1m%s\x1b[0m%s\x1b[34m%s\x1b[0m%s\x1b[33m%s\x1b[0m%s\x1b[35;1m%s\x1b[0m\n"                      // Slice输出格式 切片遍历·4部分: <切片内容1><分隔符><切片内容2><分隔符><切片内容3><分隔符><切片内容4>
+	SliceTraverse4PSuffixFormat          = "\x1b[32;1m%s\x1b[0m%s\x1b[34m%s\x1b[0m%s\x1b[33m%s\x1b[0m%s\x1b[35;1m%s\x1b[0m%s%s\n"                  // Slice输出格式 带后缀的切片遍历·4部分: <切片内容1><分隔符><切片内容2><分隔符><切片内容3><分隔符><切片内容4><分隔符><后缀>
+	SliceTraverse5PFormat                = "\x1b[32;1m%s\x1b[0m%s\x1b[34m%s\x1b[0m%s\x1b[33;1m%s\x1b[0m%s\x1b[33m%s\x1b[0m%s\x1b[35;1m%s\x1b[0m\n" // Slice输出格式 切片遍历·5部分: <切片内容1><分隔符><切片内容2><分隔符><切片内容3><分隔符><切片内容4><分隔符><切片内容5>
+
+	AskFormat = "\x1b[34;1m%s\x1b[0m" // 问询信息输出格式 问询信息: <问询信息>
+
+	SuccessFormat                = "\x1b[32;1m%s\x1b[0m\n"     // 成功信息输出格式 成功信息: <成功信息>
+	SuccessDarkFormat            = "\x1b[36;1m%s\x1b[0m\n"     // 成功信息输出格式 暗色成功信息: <成功信息>
+	SuccessNoNewLineFormat       = "\x1b[32;1m%s\x1b[0m"       // 成功信息输出格式 成功信息·不换行: <成功信息>
+	SuccessSuffixFormat          = "\x1b[32;1m%s\x1b[0m%s%s\n" // 成功信息输出格式 带后缀的成功信息: <成功信息><分隔符><后缀>
+	SuccessSuffixNoNewLineFormat = "\x1b[32;1m%s\x1b[0m%s%s"   // 成功信息输出格式 带后缀的成功信息·不换行: <成功信息><分隔符><后缀>
+
+	TipsPrefixFormat            = "%s%s\x1b[32;1m%s\x1b[0m\n"                    // 提示信息输出格式 带前缀的提示信息: <提示信息>
+	Tips2PSuffixNoNewLineFormat = "\x1b[32;1m%s\x1b[0m%s\x1b[36;1m%s\x1b[0m%s%s" // 提示信息输出格式 带后缀的提示信息·2部分·不换行: <提示信息1><分隔符><提示信息2><分隔符><后缀>
+
+	InfoFormat             = "\x1b[33;1m%s\x1b[0m\n"                        // 展示信息输出格式 展示信息: <展示信息>
+	Info2PFormat           = "\x1b[33;1m%s%s\x1b[0m\n"                      // 展示信息输出格式 展示信息·2部分: <展示信息>
+	InfoPrefixFormat       = "%s%s\x1b[33;1m%s\x1b[0m\n"                    // 展示信息输出格式 带前缀的展示信息: <前缀><分隔符><展示信息>
+	Info2PPrefixFormat     = "%s%s\x1b[33;1m%s\x1b[0m%s\x1b[35m%s\x1b[0m\n" // 展示信息输出格式 带前缀的展示信息·2部分: <前缀><分隔符><展示信息1><分隔符><展示信息2>
+	InfoSuffixFormat       = "\x1b[33;1m%s\x1b[0m%s%s\n"                    // 展示信息输出格式 带后缀的展示信息: <展示信息><分隔符><后缀>
+	InfoPrefixSuffixFormat = "%s%s\x1b[33;1m%s\x1b[0m%s%s\n"                // 展示信息输出格式 带前后缀的展示信息: <前缀><分隔符><展示信息><分隔符><后缀>
+
+	ErrorBaseFormat   = "\x1b[31m%s\x1b[0m\n"     // 错误信息输出格式 基础错误: <错误信息>
+	ErrorPrefixFormat = "%s%s\x1b[31m%s\x1b[0m\n" // 错误信息输出格式 带前缀的错误: <前缀><分隔符><错误信息>
+	ErrorSuffixFormat = "\x1b[31m%s\x1b[0m%s%s\n" // 错误信息输出格式 带后缀的错误: <错误信息><分隔符><后缀>
+)
+
+// ---------- 环境变量
+
+var Platform = runtime.GOOS                   // 操作系统
+var Arch = runtime.GOARCH                     // 系统架构
+var UserInfo, _ = GetUserInfoByName(UserName) // 用户信息
+// 用户名，当程序提权运行时，使用 SUDO_USER 变量获取提权前的用户名
+var UserName = func() string {
+	if GetVariable("SUDO_USER") != "" {
+		return GetVariable("SUDO_USER")
+	}
+	return GetVariable("USER")
+}()
+
+// 用来处理不同系统之间的变量名差异
+var platformChart = map[string]map[string]string{
+	"windows": {
+		"HOME":     "USERPROFILE",  // 用户主目录路径
+		"USER":     "USERNAME",     // 当前登录用户名
+		"SHELL":    "ComSpec",      // 默认shell或命令提示符路径
+		"PWD":      "CD",           // 当前工作目录路径
+		"HOSTNAME": "COMPUTERNAME", // 计算机主机名
+	},
+}
+
+// GetVariable 获取环境变量
+//
+// 参数：
+//   - key: 变量名
+//
+// 返回：
+//   - 变量值
+func GetVariable(key string) string {
+	if innerMap, exists := platformChart[Platform]; exists {
+		if _, variableExists := innerMap[key]; variableExists {
+			key = platformChart[Platform][key]
+		}
+	}
+	variable := os.Getenv(key)
+
+	return variable
+}
+
+// GetHostname 获取系统 HOSTNAME
+//
+// 返回：
+//   - HOSTNAME 或空字符串
+func GetHostname() string {
+	hostname, err := os.Hostname()
+	if err != nil {
+		return ""
+	}
+	return hostname
+}
+
+// SetVariable 设置环境变量
+//
+// 参数：
+//   - key: 变量名
+//   - value: 变量值
+//
+// 返回：
+//   - 错误信息
+func SetVariable(key, value string) error {
+	return os.Setenv(key, value)
+}
+
+// GetUserInfoByName 根据用户名获取用户信息
+//
+// 参数：
+//   - userName: 用户名
+//
+// 返回：
+//   - 用户信息
+//   - 错误信息
+func GetUserInfoByName(userName string) (*user.User, error) {
+	userInfo, err := user.Lookup(userName)
+	if err != nil {
+		return nil, err
+	}
+	return userInfo, nil
+}
+
+// GetUserInfoById 根据 ID 获取用户信息
+//
+// 参数：
+//   - userId: 用户 ID
+//
+// 返回：
+//   - 用户信息
+//   - 错误信息
+func GetUserInfoById(userId int) (*user.User, error) {
+	userInfo, err := user.LookupId(strconv.Itoa(userId))
+	if err != nil {
+		return nil, err
+	}
+	return userInfo, nil
+}
+
+// GetCurrentUserInfo 获取当前用户信息
+//
+// 返回：
+//   - 用户信息
+//   - 错误信息
+func GetCurrentUserInfo() (*user.User, error) {
+	currentUser, err := user.Current()
+	if err != nil {
+		return nil, err
+	}
+	return currentUser, nil
+}
