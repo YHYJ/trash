f810ac636280c6f148e5fc94bdcbc863a1a94f1e
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2024-04-11 10:05:54 +0800

    v0.3.3

--- general/version.go
+++ general/version.go
@@ -16,7 +16,7 @@ import (
 
 const (
 	Name    string = "Trash"                 // 程序名
-	Version string = "v0.3.2"                // 程序版本
+	Version string = "v0.3.3"                // 程序版本
 	Project string = "github.com/yhyj/trash" // 项目地址
 )
 
