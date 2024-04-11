d9ffa5d480c3fe8d48036e149268b8ece29d5533
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2024-04-11 13:48:07 +0800

    独立 `version` 子命令

--- cmd/version.go
+++ cmd/version.go
@@ -10,9 +10,8 @@ Description: 执行子命令 'version'
 package cmd
 
 import (
-	"github.com/gookit/color"
 	"github.com/spf13/cobra"
-	"github.com/yhyj/trash/general"
+	"github.com/yhyj/trash/cli"
 )
 
 // versionCmd represents the version command
@@ -24,17 +23,8 @@ var versionCmd = &cobra.Command{
 		// 解析参数
 		onlyFlag, _ := cmd.Flags().GetBool("only")
 
-		programInfo := general.ProgramInfo()
-
-		if onlyFlag {
-			color.Printf("%s\n", programInfo["Version"])
-		} else {
-			color.Printf("%s %s\n", general.LightText(programInfo["Name"]), general.LightText(programInfo["Version"]))
-			color.Printf("%s %s\n", general.SecondaryText("Project:"), general.SecondaryText(programInfo["Project"]))
-			color.Printf("%s %s\n", general.SecondaryText("Build rev:"), general.SecondaryText(programInfo["GitCommitHash"]))
-			color.Printf("%s %s\n", general.SecondaryText("Built on:"), general.SecondaryText(programInfo["BuildTime"]))
-			color.Printf("%s %s\n", general.SecondaryText("Built by:"), general.SecondaryText(programInfo["BuildBy"]))
-		}
+		// 打印版本信息
+		cli.PrintVersionInfo(onlyFlag)
 	},
 }
 
