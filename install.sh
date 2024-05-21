28a5871722ebec34f6dafeb8bd45e41673fa032d
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2024-05-21 10:56:13 +0800

    添加检测系统是否受支持的功能

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ Author: YJ
 Email: yj1516268@outlook.com
 Created Time: 2023-11-26 11:41:03
 
-Description: 一键安装trash
+Description: 一键安装 trash
 参考了：https://github.com/liuchengxu/space-vim
 
 Attentions:
@@ -28,34 +28,10 @@ REPO="trash"
 
 # 系统信息
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
 ATYPE="tar.gz"
-case $OS in
-  windows)
-    # 如果是Linux或macOS系统，压缩包是tar.gz格式；如果是windows系统，压缩包是zip格式
-    ATYPE="zip"
-    ;;
-  *)
-    # 如果不是已知的系统，保留原始值
-    ;;
-esac
-_ARCH=$(uname -m)
-case $_ARCH in
-  x86_64)
-    ARCH="amd64"
-    ;;
-  aarch64)
-    ARCH="arm64"
-    ;;
-  # 还需要处理arm 32bit情况
-  # armv7l)
-  #   ARCH="armv7"
-  #   ;;
-  *)
-    # 如果不是已知的架构，保留原始值
-    ;;
-esac
 
-# 最新Release信息API
+# 最新 Release 信息 API
 RELEASE_API="https://api.github.com/repos/$OWNER/$REPO/releases/latest"
 # 请求头参数
 ACCEPT="application/vnd.github+json"
@@ -71,7 +47,7 @@ PROGRAM_MODE="0755"
 PROGRAM_OWNER="root"
 PROGRAM_GROUP="root"
 PROGRAM_FILE_PATH="/usr/local/bin"
-# LICENSE文件信息
+# LICENSE 文件信息
 LICENSE_FILE="LICENSE"
 LICENSE_MODE="0644"
 LICENSE_OWNER="root"
@@ -81,27 +57,66 @@ LICENSE_FILE_PATH="/usr/local/share/licenses/$PROGRAM_FILE"
 ####################################################################
 #+++++++++++++++++++++++++ Define Function ++++++++++++++++++++++++#
 ####################################################################
+#------------------------- Exit Code Variable
+readonly err_file=1         # 文件/路径类错误
+readonly err_no_program=127 # 未找到命令
+readonly err_unsupport=202  # 不受支持
 #------------------------- Feature Function
-function exists() { # 检查命令是否存在
-  command -v "$1" > /dev/null 2>&1
-}
-
 function msg() { # 输出基础信息
   echo -e "$1" >&2
 }
 
 function success() { # 输出成功信息
-  msg "\x1b[32m[✔]\x1b[0m ${1}${2}"
+  msg "\x1b[32m[✔]\x1b[0m ${1}"
 }
 
 function error() { # 输出错误信息
-  msg "\x1b[31m[✘]\x1b[0m ${1}${2}"
-  exit 1
+  msg "\x1b[31m[✘]\x1b[0m ${1}"
+  exit "${2}"
+}
+
+function checkos() { # 检查系统
+  # 系统类型
+  case $OS in
+    linux)
+      ATYPE="tar.gz"
+      ;;
+    darwin)
+      ATYPE="tar.gz"
+      ;;
+    *)
+      # 不支持其他系统
+      error "Unsupported system: $OS" $err_unsupport
+      ;;
+  esac
+
+  # 系统架构
+  case $ARCH in
+    x86_64)
+      ARCH="amd64"
+      ;;
+    aarch64)
+      ARCH="arm64"
+      ;;
+    armv7l)
+      ARCH="armv7"
+      ;;
+    armv6l)
+      ARCH="armv6"
+      ;;
+    *)
+      # 如果不是已知的架构，保留原始值
+      ;;
+  esac
+}
+
+function exists() { # 检查命令是否存在
+  command -v "$1" > /dev/null 2>&1
 }
 
 function checkdep() { # 检查依赖
   if ! exists "$1"; then
-    error "Missing dependency: $1"
+    error "Missing dependency: $1" $err_no_program
   fi
 }
 
@@ -116,23 +131,23 @@ function goto() { # 跳转到指定目录
   cd "$1" || exit
 }
 
-function download() { # 下载文件（$1是下载链接，$2是下载的文件名, $3是下载文件大小）
+function download() { # 下载文件（$1 是下载链接，$2 是下载的文件名, $3 是下载文件大小）
   if [ -n "$1" ]; then
     if exists "pv" && [[ $3 -gt 0 ]]; then
-      # 使用pv的情况下写入文件由pv控制，curl不使用'-O'参数
+      # 使用 pv 的情况下写入文件由 pv 控制，curl 不使用 '-O' 参数
       curl -L -s "$1" | pv -s "$3" -N "Downloading $2" > "$2"
     else
       curl -LO -s "$1"
     fi
   else
-    error "No download link provided"
+    error "No download link provided" $err_file
   fi
 }
 
 function checksum() { # 校验文件（不校验找不到的文件；仅使用状态码表示结果）
   if [ -n "$1" ]; then
     if ! sha256sum --ignore-missing --status --check "$1"; then
-      error "File verification failed"
+      error "File verification failed" $err_file
     fi
   fi
 }
@@ -147,15 +162,17 @@ function installer() { # 安装程序
 ####################################################################
 #++++++++++++++++++++++++++++++ Main ++++++++++++++++++++++++++++++#
 ####################################################################
+# 检查系统
+checkos
 # 检查依赖项
 checkdep 'curl'
 checkdep 'jq'
 
-# 访问GitHub API，获取Release信息
+# 访问 GitHub API，获取 Release 信息
 response=$(curl -L -H "Accept: $ACCEPT" -s "$RELEASE_API")
 assets=$(echo "$response" | jq -r '.assets[] | {name: .name, size: .size, content_type: .content_type, download_url: .browser_download_url, download_count: .download_count}')
 
-# 获取TAG
+# 获取 TAG
 TAG=$(echo "$response" | jq -r '.tag_name')
 # 校验文件名
 CHECKSUMS_FILE="checksums.txt"
