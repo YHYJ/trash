dfc7626d7f9fac6e85b6bcc3d1874590bd124857
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2024-05-29 16:55:37 +0800

    从 general/define_variable.go 拆分出代码变量

--- general/define_recycle.go
+++ general/define_recycle.go
@@ -10,6 +10,7 @@ Description: 操作回收站
 package general
 
 import (
+	"path/filepath"
 	"time"
 
 	"github.com/gookit/color"
@@ -21,6 +22,52 @@ type FileEntry struct {
 	Time         time.Time // 文件删除时间
 	OriginalPath string    // 文件原绝对路径（即删除前的路径）
 	Path         string    // 文件绝对路径（即删除后的路径）
+}
+
+var (
+	TrashPath       = filepath.Join(UserInfo.HomeDir, "/.local/share/Trash") // 回收站路径
+	TrashFilePath   = filepath.Join(TrashPath, "files")                      // 回收站文件存储路径
+	TrashInfoPath   = filepath.Join(TrashPath, "info")                       // 已删除文件的 trashinfo 文件路径
+	DeviceTrashPath = func() string {                                        // 可移动设备回收站名
+		otherTrash := filepath.Join(".Trash", UserInfo.Uid)
+		if FileExist(otherTrash) {
+			return otherTrash
+		}
+		return color.Sprintf(".Trash-%s", UserInfo.Uid)
+	}()
+	TrashInfoFileContent    = "[Trash Info]\nPath=%s\nDeletionDate=%s\n" // 已删除文件的 trashinfo 文件内容
+	TrashInfoFileTimeFormat = "2006-01-02T15:04:05"                      // 记录文件删除时间的字符串格式
+)
+
+type StringSlice []string
+
+var fsTypes = StringSlice{ // 在此切片中的文件系统类型视为物理设备（来自 https://github.com/andreafrancia/trash-cli）
+	"btrfs",
+	"fuse", // https://github.com/andreafrancia/trash-cli/issues/250
+	"fuse.glusterfs",
+	"fuse.mergerfs", // https://github.com/andreafrancia/trash-cli/issues/255
+	"nfs",
+	"nfs4",
+	"p9", // file system used in WSL 2 (Windows Subsystem for Linux)
+}
+
+// Have 检查字符串切片中是否存在指定的字符串
+//
+// 参数：
+//   - target: 目标字符串
+//
+// 返回：
+//   - 存在返回 true，否则返回 false
+func (s StringSlice) Have(target string) bool {
+	// 遍历切片中的每个元素
+	for _, item := range s {
+		// 如果找到目标字符串，则返回 true
+		if item == target {
+			return true
+		}
+	}
+	// 如果切片中不存在目标字符串，则返回 false
+	return false
 }
 
 // CheckRecycleBin 检查回收站是否存在
--- general/define_variable.go
+++ general/define_variable.go
@@ -2,7 +2,7 @@
 File: define_variable.go
 Author: YJ
 Email: yj1516268@outlook.com
-Created Time: 2023-11-26 11:32:05
+Created Time: 2023-04-18 13:46:00
 
 Description: 操作变量
 */
@@ -15,94 +15,22 @@ import (
 	"path/filepath"
 	"runtime"
 	"strconv"
-
-	"github.com/gookit/color"
+	"strings"
 )
-
-// ---------- 代码变量
-
-var (
-	FgBlackText        = color.FgBlack.Render        // 前景色 - 黑色
-	FgWhiteText        = color.FgWhite.Render        // 前景色 - 白色
-	FgLightWhiteText   = color.FgLightWhite.Render   // 前景色 - 亮白色
-	FgGrayText         = color.FgGray.Render         // 前景色 - 灰色
-	FgRedText          = color.FgRed.Render          // 前景色 - 红色
-	FgLightRedText     = color.FgLightRed.Render     // 前景色 - 亮红色
-	FgGreenText        = color.FgGreen.Render        // 前景色 - 绿色
-	FgLightGreenText   = color.FgLightGreen.Render   // 前景色 - 亮绿色
-	FgYellowText       = color.FgYellow.Render       // 前景色 - 黄色
-	FgLightYellowText  = color.FgLightYellow.Render  // 前景色 - 亮黄色
-	FgBlueText         = color.FgBlue.Render         // 前景色 - 蓝色
-	FgLightBlueText    = color.FgLightBlue.Render    // 前景色 - 亮蓝色
-	FgMagentaText      = color.FgMagenta.Render      // 前景色 - 品红
-	FgLightMagentaText = color.FgLightMagenta.Render // 前景色 - 亮品红
-	FgCyanText         = color.FgCyan.Render         // 前景色 - 青色
-	FgLightCyanText    = color.FgLightCyan.Render    // 前景色 - 亮青色
-
-	BgBlackText        = color.BgBlack.Render        // 背景色 - 黑色
-	BgWhiteText        = color.BgWhite.Render        // 背景色 - 白色
-	BgLightWhiteText   = color.BgLightWhite.Render   // 背景色 - 亮白色
-	BgGrayText         = color.BgGray.Render         // 背景色 - 灰色
-	BgRedText          = color.BgRed.Render          // 背景色 - 红色
-	BgLightRedText     = color.BgLightRed.Render     // 背景色 - 亮红色
-	BgGreenText        = color.BgGreen.Render        // 背景色 - 绿色
-	BgLightGreenText   = color.BgLightGreen.Render   // 背景色 - 亮绿色
-	BgYellowText       = color.BgYellow.Render       // 背景色 - 黄色
-	BgLightYellowText  = color.BgLightYellow.Render  // 背景色 - 亮黄色
-	BgBlueText         = color.BgBlue.Render         // 背景色 - 蓝色
-	BgLightBlueText    = color.BgLightBlue.Render    // 背景色 - 亮蓝色
-	BgMagentaText      = color.BgMagenta.Render      // 背景色 - 品红
-	BgLightMagentaText = color.BgLightMagenta.Render // 背景色 - 亮品红
-	BgCyanText         = color.BgCyan.Render         // 背景色 - 青色
-	BgLightCyanText    = color.BgLightCyan.Render    // 背景色 - 亮青色
-
-	InfoText      = color.Info.Render      // Info 文本
-	NoteText      = color.Note.Render      // Note 文本
-	LightText     = color.Light.Render     // Light 文本
-	ErrorText     = color.Error.Render     // Error 文本
-	DangerText    = color.Danger.Render    // Danger 文本
-	NoticeText    = color.Notice.Render    // Notice 文本
-	SuccessText   = color.Success.Render   // Success 文本
-	CommentText   = color.Comment.Render   // Comment 文本
-	PrimaryText   = color.Primary.Render   // Primary 文本
-	WarnText      = color.Warn.Render      // Warn 文本
-	QuestionText  = color.Question.Render  // Question 文本
-	SecondaryText = color.Secondary.Render // Secondary 文本
-)
-
-var (
-	TrashPath       = filepath.Join(UserInfo.HomeDir, "/.local/share/Trash") // 回收站路径
-	TrashFilePath   = filepath.Join(TrashPath, "files")                      // 回收站文件存储路径
-	TrashInfoPath   = filepath.Join(TrashPath, "info")                       // 已删除文件的 trashinfo 文件路径
-	DeviceTrashPath = func() string {                                        // 可移动设备回收站名
-		otherTrash := filepath.Join(".Trash", UserID)
-		if FileExist(otherTrash) {
-			return otherTrash
-		}
-		return color.Sprintf(".Trash-%s", UserID)
-	}()
-	TrashInfoFileContent    = "[Trash Info]\nPath=%s\nDeletionDate=%s\n" // 已删除文件的 trashinfo 文件内容
-	TrashInfoFileTimeFormat = "2006-01-02T15:04:05"                      // 记录文件删除时间的字符串格式
-)
-
-type StringSlice []string
-
-var fsTypes = StringSlice{ // 在此切片中的文件系统类型视为物理设备（来自 https://github.com/andreafrancia/trash-cli）
-	"btrfs",
-	"fuse", // https://github.com/andreafrancia/trash-cli/issues/250
-	"fuse.glusterfs",
-	"fuse.mergerfs", // https://github.com/andreafrancia/trash-cli/issues/255
-	"nfs",
-	"nfs4",
-	"p9", // file system used in WSL 2 (Windows Subsystem for Linux)
-}
 
 // ---------- 环境变量
 
-var Platform = runtime.GOOS                   // 操作系统
-var Arch = runtime.GOARCH                     // 系统架构
-var UserInfo, _ = GetUserInfoByName(UserName) // 用户信息
-var UserID = UserInfo.Uid                     // 用户 ID
+// 用来处理不同系统之间的变量名差异
+var platformChart = map[string]map[string]string{
+	"windows": {
+		"HOME":     "USERPROFILE",  // 用户主目录路径
+		"USER":     "USERNAME",     // 当前登录用户名
+		"SHELL":    "ComSpec",      // 默认 shell 或命令提示符路径
+		"PWD":      "CD",           // 当前工作目录路径
+		"HOSTNAME": "COMPUTERNAME", // 计算机主机名
+	},
+}
+
 // 用户名，当程序提权运行时，使用 SUDO_USER 变量获取提权前的用户名
 var UserName = func() string {
 	if GetVariable("SUDO_USER") != "" {
@@ -111,16 +39,21 @@ var UserName = func() string {
 	return GetVariable("USER")
 }()
 
-// 用来处理不同系统之间的变量名差异
-var platformChart = map[string]map[string]string{
-	"windows": {
-		"HOME":     "USERPROFILE",  // 用户主目录路径
-		"USER":     "USERNAME",     // 当前登录用户名
-		"SHELL":    "ComSpec",      // 默认shell或命令提示符路径
-		"PWD":      "CD",           // 当前工作目录路径
-		"HOSTNAME": "COMPUTERNAME", // 计算机主机名
-	},
-}
+var Platform = runtime.GOOS                   // 操作系统
+var Arch = runtime.GOARCH                     // 系统架构
+var Sep = string(filepath.Separator)          // 路径分隔符
+var UserInfo, _ = GetUserInfoByName(UserName) // 用户信息
+var Language = GetLanguage()                  // 系统语言
+
+var (
+	programDir = strings.ToLower(Name)                      // 程序目录
+	configDir  = filepath.Join(UserInfo.HomeDir, ".config") // 配置目录
+	configFile = "config.toml"                              // 配置文件
+
+	ConfigFile = filepath.Join(configDir, programDir, configFile) // 配置文件路径
+)
+
+// ---------- 变量相关函数
 
 // GetVariable 获取环境变量
 //
@@ -138,6 +71,20 @@ func GetVariable(key string) string {
 	variable := os.Getenv(key)
 
 	return variable
+}
+
+// GetLanguage 获取系统语言
+//
+// 返回:
+//   - 系统语言，目前仅支持 zh 或 en
+func GetLanguage() string {
+	language := GetVariable("LANGUAGE")
+	if strings.Contains(language, "zh") {
+		return "zh"
+	} else {
+		return "en"
+
+	}
 }
 
 // GetHostname 获取系统 HOSTNAME
@@ -207,17 +154,4 @@ func GetCurrentUserInfo() (*user.User, error) {
 		return nil, err
 	}
 	return currentUser, nil
-}
-
-// Have 检查字符串切片中是否存在指定的字符串
-func (s StringSlice) Have(target string) bool {
-	// 遍历切片中的每个元素
-	for _, item := range s {
-		// 如果找到目标字符串，则返回 true
-		if item == target {
-			return true
-		}
-	}
-	// 如果切片中不存在目标字符串，则返回 false
-	return false
 }
