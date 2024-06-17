a3e2edcfc101dfee1f29caba9c812e509bf44a02
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2024-06-17 11:33:35 +0800

    sync.sh 新增处理脚本功能

--- sync.sh
+++ sync.sh
@@ -20,21 +20,24 @@ script_dir=$(dirname "$0") # 本脚本所在路径
 repo_dir="${HOME}/Documents/Repos" # 本地源库路径
 tmp_dir="/tmp/sync-hook"           # 临时源库路径
 
-source_hooks_dir="System/git/hooks" # git hook 来源路径
-hooks_dir="${script_dir}/git/hooks" # git hook 本库路径
+source_dir="System/git"      # 来源路径
+save_dir="${script_dir}/git" # 本库路径
 
 repo_name="git@github.com:YHYJ/System.git" # 云端 System 存储库
 
-mkdir -p "${hooks_dir}"
+mkdir -p "${save_dir}"
 
-if [ -d "${repo_dir}/${source_hooks_dir}" ]; then
+if [ -d "${repo_dir}/${source_dir}" ]; then
   echo "Synchronizing hooks from local 'System' repo"
-  cp -r "${repo_dir}/${source_hooks_dir}/"* "${hooks_dir}"
+  cp -r "${repo_dir}/${source_dir}/"* "${save_dir}"
   echo "Hooks copied successfully"
 else
   echo "Synchronizing hooks from cloud 'System' repo"
   git clone "${repo_name}" "${tmp_dir}/System"
-  cp -r "${tmp_dir}/${source_hooks_dir}/"* "${hooks_dir}"
+  cp -r "${tmp_dir}/${source_dir}/"* "${save_dir}"
   rm -rf "${tmp_dir}"
   echo "Hooks copied successfully"
 fi
+
+cp -r "${save_dir}/scripts/"* "${PWD}"
+rm -rf "${save_dir}/scripts"
