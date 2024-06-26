1a724cb0202311c11cf58a2352d259d1a451a9ef
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2024-06-26 16:43:06 +0800

    清除未使用的函数和变量

--- general/define_filemanager.go
+++ general/define_filemanager.go
@@ -11,11 +11,9 @@ package general
 
 import (
 	"bufio"
-	"io"
 	"os"
 	"path/filepath"
 	"strings"
-	"sync"
 	"syscall"
 
 	"github.com/gookit/color"
@@ -39,37 +37,6 @@ func GetFsID(file string) (uint64, error) {
 	id := fileInfo.Sys().(*syscall.Stat_t).Dev
 
 	return uint64(id), nil
-}
-
-// ReadFileLine 读取文件指定行
-//
-// 参数：
-//   - file: 文件路径
-//   - line: 行号
-//
-// 返回：
-//   - 指定行的内容
-func ReadFileLine(file string, line int) string {
-	// 打开文件
-	text, err := os.Open(file)
-	if err != nil {
-		fileName, lineNo := GetCallerInfo()
-		color.Printf("%s %s -> Unable to open file: %s\n", DangerText("Error:"), SecondaryText("[", fileName, ":", lineNo+1, "]"), err)
-	}
-	defer text.Close()
-
-	// 创建一个扫描器对象按行遍历
-	scanner := bufio.NewScanner(text)
-	// 行计数
-	count := 1
-	// 逐行读取，输出指定行
-	for scanner.Scan() {
-		if line == count {
-			return scanner.Text()
-		}
-		count++
-	}
-	return ""
 }
 
 // ReadFileKey 读取文件包含关键字的行
@@ -98,36 +65,6 @@ func ReadFileKey(file, key string) string {
 		}
 	}
 	return ""
-}
-
-// ReadFileCount 获取文件包含关键字的行的计数
-//
-// 参数：
-//   - file: 文件路径
-//   - key: 关键字
-//
-// 返回：
-//   - 包含关键字的行的数量
-func ReadFileCount(file, key string) int {
-	// 打开文件
-	text, err := os.Open(file)
-	if err != nil {
-		fileName, lineNo := GetCallerInfo()
-		color.Printf("%s %s -> Unable to open file: %s\n", DangerText("Error:"), SecondaryText("[", fileName, ":", lineNo+1, "]"), err)
-	}
-	defer text.Close()
-
-	// 创建一个扫描器对象按行遍历
-	scanner := bufio.NewScanner(text)
-	// 计数器
-	count := 0
-	// 逐行读取，输出指定行
-	for scanner.Scan() {
-		if strings.Contains(scanner.Text(), key) {
-			count++
-		}
-	}
-	return count
 }
 
 // FileExist 判断文件是否存在
@@ -197,28 +134,6 @@ func FileEmpty(file string) bool {
 	return fi.Size() == 0
 }
 
-// FolderEmpty 判断文件夹是否为空
-//
-//   - 包括隐藏文件
-//
-// 参数：
-//   - dir: 文件夹路径
-//
-// 返回：
-//   - 文件夹为空返回 true，否则返回 false
-func FolderEmpty(dir string) bool {
-	text, err := os.Open(dir)
-	if err != nil {
-		return true
-	}
-	defer text.Close()
-
-	if _, err = text.Readdir(1); err == io.EOF {
-		return true
-	}
-	return false
-}
-
 // CreateFile 创建文件，包括其父目录
 //
 // 参数：
@@ -255,17 +170,6 @@ func CreateDir(dir string) error {
 		return nil
 	}
 	return os.MkdirAll(dir, os.ModePerm)
-}
-
-// GoToDir 进到指定文件夹
-//
-// 参数：
-//   - dirPath: 文件夹路径
-//
-// 返回：
-//   - 错误信息
-func GoToDir(dirPath string) error {
-	return os.Chdir(dirPath)
 }
 
 // WriteFile 写入内容到文件，文件不存在则创建，不自动换行
@@ -307,121 +211,4 @@ func DeleteFile(filePath string) error {
 		return nil
 	}
 	return os.RemoveAll(filePath)
-}
-
-// CompareFile 并发比较两个文件是否相同
-//
-// 参数：
-//   - file1Path: 文件1路径
-//   - file2Path: 文件2路径
-//
-// 返回：
-//   - 文件相同返回 true，出错或不同返回 false
-func CompareFile(file1Path string, file2Path string) (bool, error) {
-	// 尝试打开文件
-	file1, err := os.Open(file1Path)
-	if err != nil {
-		return false, err
-	}
-	defer file1.Close()
-	file2, err := os.Open(file2Path)
-	if err != nil {
-		return false, err
-	}
-	defer file2.Close()
-
-	// 获取文件大小
-	file1Info, err := file1.Stat()
-	if err != nil {
-		return false, err
-	}
-	file2Info, err := file2.Stat()
-	if err != nil {
-		return false, err
-	}
-	file1Size := file1Info.Size()
-	file2Size := file2Info.Size()
-
-	// 如果文件大小不同则直接返回
-	if file1Size != file2Size {
-		return false, nil
-	}
-
-	// 文件大小相同则（按块）比较文件内容
-	const chunkSize = 1024 * 1024                             // 每次比较的块大小（1MB）
-	numChunks := int((file1Size + chunkSize - 1) / chunkSize) // 计算文件需要被分成多少块
-
-	equal := true                // 文件是否相同的标志位
-	var wg sync.WaitGroup        // wg 用于等待所有的 goroutine 执行完毕，然后关闭 errCh 通道
-	errCh := make(chan error, 1) // errCh 用于接收 goroutine 执行过程中返回的错误
-
-	for i := 0; i < numChunks; i++ { // 同时比较多个块
-		wg.Add(1)
-		go func(chunkIndex int) {
-			defer wg.Done()
-
-			// 计算当前块的偏移量和大小
-			offset := int64(chunkIndex) * chunkSize
-			size := chunkSize
-			if offset+int64(size) > file1Size {
-				size = int(file1Size - offset)
-			}
-
-			// 创建两个大小为 size 的 buffer
-			buffer1 := make([]byte, size)
-			buffer2 := make([]byte, size)
-
-			// 从文件中读取指定大小的内容到 buffer
-			if _, err := file1.ReadAt(buffer1, offset); err != nil && err != io.EOF {
-				errCh <- err
-				return
-			}
-
-			// 从文件中读取指定大小的内容到 buffer
-			if _, err = file2.ReadAt(buffer2, offset); err != nil && err != io.EOF {
-				errCh <- err
-				return
-			}
-
-			// 比较两个 buffer 是否相同
-			if !bytesEqual(buffer1, buffer2) {
-				equal = false
-			}
-		}(i)
-	}
-
-	go func() {
-		wg.Wait()
-		close(errCh)
-	}()
-
-	for err := range errCh {
-		if err != nil {
-			return false, err
-		}
-	}
-
-	return equal, nil
-}
-
-// bytesEqual 比较两个文件的内容
-//
-// 参数：
-//   - b1: 文件1内容
-//   - b2: 文件2内容
-//
-// 返回：
-//   - 相同返回 true，不同返回 false
-func bytesEqual(b1 []byte, b2 []byte) bool {
-	if len(b1) != len(b2) {
-		return false
-	}
-
-	for i := 0; i < len(b1); i++ {
-		if b1[i] != b2[i] {
-			return false
-		}
-	}
-
-	return true
 }
